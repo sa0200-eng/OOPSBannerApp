f85f9a3fcd67df3260816fdd3f653fb28cfbb55c
sa0200-eng <sa0200@srmist.edu.in>
2026-03-03 10:32:19 +0530

    Added UC6 - Refactored Banner Logic using Static Helper Methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,55 @@
-public class OOPSBannerApp {
+public class BannerApp {
+
+    // Method for O
+    public static String[] getOPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Method for P
+    public static String[] getPPattern() {
+        return new String[]{
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Method for S
+    public static String[] getSPattern() {
+        return new String[]{
+            " ******",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ******"
+        };
+    }
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join("  ", " ***** ", " ***** ", " ***** ", " ***** "),
-            String.join("  ", "*     *", "*     *", "*     *", "*      "),
-            String.join("  ", "*     *", "*     *", "*     *", "*      "),
-            String.join("  ", "*     *", "*     *", " ***** ", " ***** "),
-            String.join("  ", "*     *", "*     *", "*       ", "      *"),
-            String.join("  ", "*     *", "*     *", "*       ", "*     *"),
-            String.join("  ", " ***** ", " ***** ", "*       ", " ***** ")
-        };
+        String[] O = getOPattern();
+        String[] P = getPPattern();
+        String[] S = getSPattern();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = O[i] + "  " + O[i] + "  " + P[i] + "  " + S[i];
+        }
 
         for (String line : banner) {
             System.out.println(line);
